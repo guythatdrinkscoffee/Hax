1191c17a39ec2c27ee84a982712ffcf268853726
J.Manuel Zaragoza <zaragozajn@gmail.com>
2022-04-27 12:16:38 -0700

    Initial Commit

---
+++ Shared/HaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaxApp.swift
+//  Shared
+//
+//  Created by J Manuel Zaragoza on 4/27/22.
+//
+
+import SwiftUI
+
+@main
+struct HaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
